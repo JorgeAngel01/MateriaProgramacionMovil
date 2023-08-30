a4789346c0469708c287938bd7577cd95d3d7e39
Angel <jorge.angelus0107@gmail.com>
2023-08-30 15:12:55 -0600

    feature(practica4)

---
+++ unidad1/tarea4/FirstProject/src/theme/colors.js
@@ -0,0 +1,12 @@
+export const THEME = {
+    COLORS:{
+        DARK_BLUE: '#0c1c2c',
+        GREY: '#7c8c9b',
+        WHITE: '#ffffff',
+        GREEN: {
+            LIGHT: '#c5dd9c',
+            DARK: '#c0e66b',
+        }
+
+    }
+}
