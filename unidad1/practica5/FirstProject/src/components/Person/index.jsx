00a992ba51dfd8004119f3de0a97111a861a9f50
Angel <jorge.angelus0107@gmail.com>
2023-08-31 09:01:27 -0600

    feature(practica5)

--- unidad1/practica5/FirstProject/src/components/Person/index.jsx
+++ unidad1/practica5/FirstProject/src/components/Person/index.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import {
+    View,
+    Text,
+  } from "react-native";
 
 const Person = ({ name, lastName, index}) => {
   return (
     <View style={{ borderWidth: 1, borderColor: index % 2 ? "red" : "blue" }}>
-      <Text style={{ fontSize: 40 }}>{props.name}</Text>
-      <Text style={{ fontSize: 40 }}>{props.lastName}</Text>
+      <Text style={{ fontSize: 40 }}>{name}</Text>
+      <Text style={{ fontSize: 40 }}>{lastName}</Text>
     </View>
   );
 };
