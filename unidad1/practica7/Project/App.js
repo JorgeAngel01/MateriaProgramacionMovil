0e2d5723168bd26e3453038361ca6c601ed0fd55
Angel <jorge.angelus0107@gmail.com>
2023-09-06 09:02:58 -0600

    feature(practica7)

---
+++ unidad1/practica7/Project/App.js
@@ -0,0 +1,33 @@
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  View,
+  Text,
+  StatusBar as ReactStatus,
+  Platform,
+} from "react-native";
+import { THEME } from "./src/theme/styles";
+import Constants from "expo-constants";
+import WelcomeScreen from "./src/screens/WelcomeScreen";
+
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <WelcomeScreen/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'White',
+    paddingTop: Constants.statusBarHeight,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "white",
+  },
+});
